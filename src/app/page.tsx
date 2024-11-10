5edc1ba5945ff68c78ea636fc8489e2a76547239
Luiz Gasparetto <luizegasp@gmail.com>
2024-11-09 23:45:08 -0300

    feat: change img to Image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import TextBadge from './components/badges/TextBadge';
 import PrimaryButton from './components/buttons/PrimaryButton';
 import SecondaryButton from './components/buttons/SecondaryButton';
 import Navbar from './components/navbar/Navbar';
+import Image from 'next/image'
 
 
 
@@ -11,28 +12,36 @@ export default function Home() {
   return (
     <div className="flex flex-col min-h-screen">
       <Navbar />
-      <Stars />
-      <PrimarySection />
+
+      <div className="flex flex-col flex-1 max-w-[1280px] mx-auto w-full">
+        <Stars />
+        <PrimarySection />
+        <SecondarySection />
+      </div>
     </div>
   );
 }
 
-
 function Stars() {
   return (
-    <div>
-      <img
-        src="svg/figma-4-stars.svg"
+    <div className="relative">
+      <Image
+        src="/svg/figma-4-stars.svg"
         alt="Star"
-        className="fixed top-[35rem] left-48 max-medium:left-16  w-24 h-24 opacity-90"
+        className="absolute top-[28rem] left-16 max-medium:left-16 w-24 h-24 opacity-90"
+        width={96}
+        height={96}
+        priority
       />
 
-      <img
-        src="svg/figma-4-stars.svg"
+      <Image
+        src="/svg/figma-4-stars.svg"
         alt="Star"
-        className="fixed top-48 right-48 max-medium:right-16 w-24 h-24 opacity-90"
+        className="absolute top-48 right-16 max-medium:right-16 w-24 h-24 opacity-90"
+        width={96}
+        height={96}
+        priority
       />
-
     </div>
   );
 }
@@ -67,9 +76,18 @@ function PrimarySection() {
 }
 
 
+function SecondarySection() {
+  return (
+    <div>
+      <TextBadge title="Resultados" />
+    </div>
+  )
+}
+
+
 const getBackgroundStyle = () => ({
   backgroundImage: "url('svg/lines.svg')",
-  backgroundSize: "80%",
+  backgroundSize: "80rem",
   backgroundPosition: "top",
   backgroundRepeat: "no-repeat",
   zIndex: -1
