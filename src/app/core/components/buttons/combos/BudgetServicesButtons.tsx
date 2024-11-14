75e8285cd5a9d46ee0389188d35b1a323c0c649f
Luiz Gasparetto <luizegasp@gmail.com>
2024-11-14 11:07:50 -0300

    feat: add device utils

--- src/app/core/components/buttons/combos/BudgetServicesButtons.tsx
+++ src/app/core/components/buttons/combos/BudgetServicesButtons.tsx
@@ -1,25 +1,29 @@
 import { useIsMobile } from "@/app/core/constants/mediaQueryConstants";
 import SecondaryButton from "../SecondaryButton";
 import PrimaryButton from "../PrimaryButton";
+import Device from "@/app/core/utils/device";
 
 interface BudgetServicesButtonsProps {
   hideServicesButton?: boolean;
 }
 
-
 export default function BudgetServicesButtons({hideServicesButton = false}: BudgetServicesButtonsProps) {
   return (
-    <div className="flex flex-col sm:flex-row space-y-4 xs:space-y-0 space-x-0 xs:space-x-4 mt-8 w-full xs:w-auto">
-      <PrimaryButton
-        title="Solicite um Orçamento"
-        fullWidth={useIsMobile()}
-        onClick={() => alert("Button clicked!")}
-      />
-      {!hideServicesButton && <SecondaryButton
-        title="Conheça nossos Serviços"
-        fullWidth={hideServicesButton}
-        onClick={() => alert("Button clicked!")}
-      />}
-    </div>
+    <Device>
+      {({ isMobile }) => {
+        return <div className="flex flex-col sm:flex-row space-y-4 xs:space-y-0 space-x-0 xs:space-x-4 mt-8 w-full xs:w-auto">
+          <PrimaryButton
+            title="Solicite um Orçamento"
+            fullWidth={isMobile || useIsMobile()}
+            onClick={() => alert("Button clicked!")}
+          />
+          {!hideServicesButton && <SecondaryButton
+            title="Conheça nossos Serviços"
+            fullWidth={hideServicesButton}
+            onClick={() => alert("Button clicked!")}
+          />}
+        </div>
+      }}
+    </Device>
   )
 }
