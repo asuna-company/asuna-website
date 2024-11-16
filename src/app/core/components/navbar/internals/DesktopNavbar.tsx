0d028a0dd7ece433b484d53defcab3218eb9b17b
zanelladev <arturf.zanella@gmail.com>
2024-11-16 17:26:16 -0300

    fix: desktop nav and foot width

--- src/app/core/components/navbar/internals/DesktopNavbar.tsx
+++ src/app/core/components/navbar/internals/DesktopNavbar.tsx
@@ -3,10 +3,16 @@ import Link from "next/link";
 import PrimaryButton from "../../buttons/PrimaryButton";
 
 function getLinkClasses() {
-  return  "text-gray-700 hover:text-blue-600 hover:underline hover:underline-offset-4 text-p2 font-poppins font-medium ";
+  return "text-gray-700 hover:text-blue-600 hover:underline hover:underline-offset-4 text-p2 font-poppins font-medium ";
 }
 
-const NavbarLink = ({ href, children }: { href: string; children: React.ReactNode }) => (
+const NavbarLink = ({
+  href,
+  children,
+}: {
+  href: string;
+  children: React.ReactNode;
+}) => (
   <li>
     <Link href={href} className={getLinkClasses()}>
       {children}
@@ -17,18 +23,22 @@ const NavbarLink = ({ href, children }: { href: string; children: React.ReactNod
 export default function DesktopNavbar() {
   return (
     <div className="bg-[var(--foreground)]">
-      <nav className="max-w-[1480px] mx-auto"> 
-        <div className="container mx-auto px-4 py-4 flex items-center justify-between">
-       
+      <nav className="max-w-[1440px] w-[90%] mx-auto flex flex-col md:flex-row ${align} justify-between">
+        <div className="container mx-auto  py-4 flex items-center justify-between">
           <Link href="/">
-            <Image src="svg/logo_with_text.svg" alt="Asuna" width={175} height={45} />
+            <Image
+              src="svg/logo_with_text.svg"
+              alt="Asuna"
+              width={175}
+              height={45}
+            />
           </Link>
 
           <ul className="flex space-x-8 items-center pt-1">
             <NavbarLink href="/">Home</NavbarLink>
             <NavbarLink href="/services">Nossos Serviços</NavbarLink>
             <NavbarLink href="/about">Sobre a Asuna</NavbarLink>
-            
+
             <PrimaryButton
               title="Entre em Contato"
               onClick={() => alert("Button clicked!")}
