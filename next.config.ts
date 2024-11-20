ca0e06d41d8c78e18af1ecce4762db702d5df0af
Luiz Gasparetto <luizegasp@gmail.com>
2024-11-20 16:52:56 -0300

    config: update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,28 +1,33 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  compress: true,
-  distDir: 'build',
-  output: 'export',
-  poweredByHeader: false,
-  trailingSlash: false,
-  staticPageGenerationTimeout: 60,
+  compress: true, 
+  distDir: 'build', 
+  poweredByHeader: false, 
+  trailingSlash: false, 
+  staticPageGenerationTimeout: 60, 
   crossOrigin: "anonymous",
-  images: { domains: ['www.asuna.com.br'] },
-  compiler: { removeConsole: true },
+  images: { 
+    domains: ['www.asuna.com.br'], 
+    unoptimized: true, 
+  },
+  compiler: { 
+    removeConsole: true, 
+  },
   experimental: {
     externalDir: true,
     typedRoutes: false,
     nextScriptWorkers: true,
     optimizeCss: {
       minify: true,
-      treeShaking: true,
+      treeShaking: true, 
     },
     staleTimes: {
       dynamic: 30,
       static: 180,
     },
   },
+
 };
 
 export default nextConfig;
