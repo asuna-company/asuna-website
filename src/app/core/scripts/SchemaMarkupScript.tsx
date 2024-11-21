e34353dfdb8c845db804d1629d7da7c7a477eebb
Luiz Gasparetto <luizegasp@gmail.com>
2024-11-21 16:42:28 -0300

    config: update schema markups and font constants

---
+++ src/app/core/scripts/SchemaMarkupScript.tsx
@@ -0,0 +1,67 @@
+import Script from "next/script";
+
+export default function SchemaMarkupScript() {
+  return (
+    <Script
+      id="schema-markup"
+      type="application/ld+json"
+      dangerouslySetInnerHTML={{
+        __html: JSON.stringify([
+          {
+            "@context": "https://schema.org",
+            "@type": "Organization",
+            "name": "Asuna",
+            "url": "https://www.asuna.com.br",
+            "logo": "https://www.asuna.com.br/images/logo.webp",
+            "description": "A Asuna é uma software house especializada em desenvolvimento mobile, consultoria e SaaS para pequenas e médias empresas no Brasil.",
+            "foundingDate": "2024-10-01",
+            "sameAs": [
+              "https://www.linkedin.com/company/asuna"
+            ],
+            "contactPoint": [
+              {
+                "@type": "ContactPoint",
+                "telephone": "+55-47-99284-7889",
+                "contactType": "Customer Service",
+                "areaServed": "BR",
+                "availableLanguage": ["Portuguese"]
+              }
+            ],
+            "address": {
+              "@type": "PostalAddress",
+              "streetAddress": "Avenida Paulista, 1106",
+              "addressLocality": "São Paulo",
+              "addressRegion": "SP",
+              "postalCode": "01310-914",
+              "addressCountry": "BR"
+            },
+            "hoursAvailable": {
+              "@type": "OpeningHoursSpecification",
+              "dayOfWeek": [
+                "Monday",
+                "Tuesday",
+                "Wednesday",
+                "Thursday",
+                "Friday"
+              ],
+              "opens": "09:00",
+              "closes": "18:00"
+            }
+          },
+          {
+            "@context": "https://schema.org",
+            "@type": "BreadcrumbList",
+            "itemListElement": [
+              {
+                "@type": "ListItem",
+                "position": 1,
+                "name": "Serviços",
+                "item": "https://www.asuna.com.br/services"
+              },
+            ]
+          }
+        ])
+      }}
+    />
+  );
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,29 +1,15 @@
 
 import "./globals.css";
 
-import Script from "next/script";
-
 import { AppRouterProvider } from "./core/configs/appRouter";
 import { SpeedInsights } from "@vercel/speed-insights/next"
 
 import { metadata } from "./core/configs/metadata";
 import { reportWebVitals } from "./core/configs/reportWebVitals";
+import SchemaMarkupScript from "./core/scripts/SchemaMarkupScript";
+import { inter, poppins } from "./core/constants/fontConstants";
 
 export { metadata };
-
-import { Poppins, Inter } from "next/font/google";
-
-const poppins = Poppins({
-  subsets: ["latin"],
-  weight: ["400", "500", "600", "700"],
-  display: "swap", 
-});
-
-const inter = Inter({
-  subsets: ["latin"],
-  weight: ["400", "500", "600", "700"],
-  display: "swap",
-});
 
 if (typeof window !== "undefined") {
   window.addEventListener("load", () => {
@@ -52,31 +38,4 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       </body>
     </html>
   );
-}
-
-
-function SchemaMarkupScript() {
-  return <Script
-    id="schema-markup"
-    type="application/ld+json"
-    dangerouslySetInnerHTML={{
-      __html: JSON.stringify({
-        "@context": "https://schema.org",
-        "@type": "Organization",
-        "name": "Asuna",
-        "url": "https://www.asuna.com.br",
-        "logo": "https://www.asuna.com.br/images/logo.webp",
-        "sameAs": [
-          "https://www.linkedin.com/company/asuna",
-        ],
-        "contactPoint": {
-          "@type": "ContactPoint",
-          "telephone": "+55-47-99284-7889",
-          "contactType": "Customer Service",
-          "areaServed": "BR",
-          "availableLanguage": ["Portuguese"]
-        }
-      }),
-    }}
-  />
 }
