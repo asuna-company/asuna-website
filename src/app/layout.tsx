190fafcb483faee53c48be2314a827a9aca8ae73
Luiz Gasparetto <luizegasp@gmail.com>
2024-11-26 14:09:02 -0300

    chore: add gtm config to root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
-
 import "./globals.css";
+import Script from "next/script";
 
 import { AppRouterProvider } from "./core/configs/appRouter";
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 import { metadata } from "./core/configs/metadata";
 import { reportWebVitals } from "./core/configs/reportWebVitals";
@@ -26,11 +26,35 @@ if (typeof window !== "undefined") {
   });
 }
 
+const GTM_ID = "GTM-NL7JPZNT"; 
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="pt-BR" className={`${poppins.style} ${inter.style}`}>
-      <SchemaMarkupScript/>
       <body>
+        <Script
+          id="google-tag-manager"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': 
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','${GTM_ID}');
+            `,
+          }}
+        />
+        <noscript>
+          <iframe
+            src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+
+        <SchemaMarkupScript />
         <AppRouterProvider>
           {children}
           <SpeedInsights />
